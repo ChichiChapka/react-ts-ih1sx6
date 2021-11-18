ee101f1872eb95dcb22e8407cae6655d10335cfa
ChichiChapka <anibuki@live.com>
2021-11-18 13:17:43 +0000

    1 file modified

--- MedicalTreatment.js
+++ MedicalTreatment.js
@@ -1,4 +1,13 @@
 import React from 'react';
 function Treatments (prop){
-  
+  function toString(prop){
+    return"[" + " treatId = " + prop.treatId+         ",treatCourseId ="
+     +prop.treatCourseId+ ", type =" +prop.type+      ", category =" +prop.category+ ", name =" +prop.name+ ",startDate =" +prop.startDate+"]";
+  }
+  return (
+    <div>
+      {toString(prop)};
+    </div>
+  );
 }
+export default Treatments;

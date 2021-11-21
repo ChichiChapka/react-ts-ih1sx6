9484e003d81db5c10c90826beb8169490c1252c1
ChichiChapka <anibuki@live.com>
2021-11-21 16:35:07 +0000

    1 file modified

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Treatments from '/.MedicalTreatment';
+import Treatments from '/.MedicalTreatment.js';
 
 
 function App(){
@@ -13,3 +13,4 @@ function App(){
 }
 export default App;
 
+ReactDOM.render (treatments, getDocumentById ('app'));
